999d42c9faae44133c237c78a0b28a8176ce20ef
Michael Kardjian <makardjian@gmail.com>
2018-11-07 21:43:40 -0800

    completed solo sprint with all tests passing for the 5 different classes

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	var someInstance = Object.create(queueMethods);
+	someInstance.storage = {};
+  	someInstance.front = 0;
+  	someInstance.back = 0;
+  	return someInstance;
 };
 
 var queueMethods = {};
 
+
+queueMethods.enqueue = function(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  };
+
+  queueMethods.dequeue = function() {
+    if (this.back > this.front) {
+      var result = this.storage[this.front];
+      delete(this.storage[this.front]);
+      this.front++;
+      return result;
+    }
+  };
+
+  queueMethods.size = function() {
+    return (this.back - this.front)
+
+  };
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	this.storage = {};
+	this.size1 = 0
 };
 
 
+Stack.prototype.push = function(value) {
+    this.storage[this.size1] = value;
+    this.size1++;
+  }
+
+
+Stack.prototype.pop = function() {
+    if (this.size1 !== 0) {
+      this.size1--;
+
+      var result = this.storage[this.size1];
+      delete this.storage[this.size1];
+      return result;
+    }
+  }
+
+Stack.prototype.size = function() {
+    return this.size1;
+  }
